4105e662c68c8e5e6d116bda27f7780f64c7731a
Huy <h.qt193@gmail.com>
2023-01-24 11:42:25 -0500

    install axios, get all index from api

--- controllers/trackerController.js
+++ controllers/trackerController.js
@@ -10,7 +10,7 @@ const Team = require('../models/team')
 // Create router
 ////////////////////////////////////////////
 const router = express.Router()
-
+const axios = require('axios')
 
 module.exports = router
 
@@ -20,18 +20,39 @@ router.get('/', (req, res) => {
     res.render('tracker/home', { loggedIn, username, userId })
 })
 
-//search players and teams 
+//search form for players and teams 
 router.get('/search', (req, res) => {
     const { username, userId, loggedIn } = req.session
     res.render('tracker/search', { loggedIn, username, userId })
 })
 
+// show tracker
 router.get('/mine', (req, res) => {
     const { username, userId, loggedIn } = req.session
     res.render('tracker/mine', { loggedIn, username, userId })
 })
 
+// get all players
 router.get('/all', (req, res) => {
     const { username, userId, loggedIn } = req.session
-    res.render('tracker/all', { loggedIn, username, userId })
+    // res.render('tracker/all', { loggedIn, username, userId })
+    axios.get('https://balldontlie.io/api/v1/players')
+        .then(data => {
+            const players = data.data
+            res.render('tracker/all', { loggedIn, username, userId, players })
+        })
+})
+
+// router.
+
+// show a player after search
+router.get('/search/:playerName', (req, res) => {
+    const { username, userId, loggedIn } = req.session
+    res.render('tracker/player', { loggedIn, username, userId })
+})
+
+//show a team after search
+router.get('/search/:teamName', (req, res) => {
+    const { username, userId, loggedIn } = req.session
+    res.render('tracker/team', { loggedIn, username, userId })
 })
